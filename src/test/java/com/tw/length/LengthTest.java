f907b73b9f0fe6c7b0abd8e148c9b088bd86ffed
canwang <nonpool@163.com>
2019-06-25 21:22:38 +0800

    rename some variables and add a new package

--- src/test/java/com/tw/length/LengthTest.java
+++ src/test/java/com/tw/length/LengthTest.java
@@ -1,4 +1,4 @@
-package com.tw;
+package com.tw.length;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,11 +22,11 @@ public class LengthTest {
   @Test
   void should_return_5_is_longer_than_3_when_compare_given_two_length_with_one_is_5_and_the_other_is_3() {
     // given
-    Length shorterLength = new Length(5);
-    Length longerLength = new Length(3);
+    Length longerLength = new Length(5);
+    Length shorterLength = new Length(3);
 
     // when
-    String compareResult = shorterLength.compare(longerLength);
+    String compareResult = longerLength.compare(shorterLength);
 
     // then
     assertEquals(compareResult, "5 is longer than 3");
@@ -35,11 +35,11 @@ public class LengthTest {
   @Test
   void should_return_5_is_as_long_as_5_when_compare_given_two_length_with_one_is_5_and_the_other_is_5() {
     // given
-    Length shorterLength = new Length(5);
-    Length longerLength = new Length(5);
+    Length length = new Length(5);
+    Length otherLength = new Length(5);
 
     // when
-    String compareResult = shorterLength.compare(longerLength);
+    String compareResult = length.compare(otherLength);
 
     // then
     assertEquals(compareResult, "5 is as long as 5");
