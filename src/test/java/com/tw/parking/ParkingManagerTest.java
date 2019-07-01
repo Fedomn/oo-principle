ab4a7818b239fb1d3868b53ac7f1bb1610e5a814
canwang <nonpool@163.com>
2019-07-01 21:18:58 +0800

    completed park part for parking manager

--- src/test/java/com/tw/parking/ParkingManagerTest.java
+++ src/test/java/com/tw/parking/ParkingManagerTest.java
@@ -25,4 +25,44 @@ public class ParkingManagerTest {
     assertThrows(InvalidTicketException.class, () -> parkingLotB.pick(ticket));
     assertThrows(InvalidTicketException.class, () -> parkingLotC.pick(ticket));
   }
+
+  @Test
+  void should_parked_car_through_smart_parking_boy_and_return_a_invalid_ticket_when_park_car_given_a_parking_manager_who_have_a_graduate_parking_boy_who_managed_parking_lot_A_without_available_space_and_a_smart_parking_boy_who_managed_parking_log_B_with_available_space_and_a_parking_lot_C() {
+    ParkingLot parkingLotA = buildParkingLotWithoutAvailableSpace();
+    GraduateParkingBoy graduateParkingBoy = new GraduateParkingBoy(parkingLotA);
+    ParkingLot parkingLotB = new ParkingLot(1);
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotB);
+    ParkingLot parkingLotC = new ParkingLot(1);
+    ParkingManager parkingManager = new ParkingManager(graduateParkingBoy, smartParkingBoy, parkingLotC);
+
+    Ticket ticket = parkingManager.park(new Car());
+
+    assertNotNull(ticket);
+    assertNotNull(parkingLotB.pick(ticket));
+    assertThrows(InvalidTicketException.class, () -> parkingLotA.pick(ticket));
+    assertThrows(InvalidTicketException.class, () -> parkingLotC.pick(ticket));
+  }
+
+  @Test
+  void should_parked_car_self_and_return_a_invalid_ticket_when_park_car_given_a_parking_manager_who_have_a_graduate_parking_boy_who_managed_parking_lot_A_without_available_space_and_a_smart_parking_boy_who_managed_parking_log_B_without_available_space_and_a_parking_lot_C_with_available_space() {
+    ParkingLot parkingLotA = buildParkingLotWithoutAvailableSpace();
+    GraduateParkingBoy graduateParkingBoy = new GraduateParkingBoy(parkingLotA);
+    ParkingLot parkingLotB = buildParkingLotWithoutAvailableSpace();
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotB);
+    ParkingLot parkingLotC = new ParkingLot(1);
+    ParkingManager parkingManager = new ParkingManager(graduateParkingBoy, smartParkingBoy, parkingLotC);
+
+    Ticket ticket = parkingManager.park(new Car());
+
+    assertNotNull(ticket);
+    assertNotNull(parkingLotC.pick(ticket));
+    assertThrows(InvalidTicketException.class, () -> parkingLotB.pick(ticket));
+    assertThrows(InvalidTicketException.class, () -> parkingLotC.pick(ticket));
+  }
+
+  private ParkingLot buildParkingLotWithoutAvailableSpace() {
+    ParkingLot parkingLot = new ParkingLot(1);
+    parkingLot.park(new Car());
+    return parkingLot;
+  }
 }
